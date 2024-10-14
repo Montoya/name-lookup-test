e4075d58f26a8f9cd5339ace94743a0f02118282
Montoya <sirokai@gmail.com>
2024-10-13 21:49:00 -0400

    dead

--- packages/snap/src/index.tsx
+++ packages/snap/src/index.tsx
@@ -1,43 +1,40 @@
-import type { OnRpcRequestHandler } from '@metamask/snaps-sdk';
-import { Box, Text, Bold } from '@metamask/snaps-sdk/jsx';
+import type {
+  OnNameLookupHandler,
+} from '@metamask/snaps-sdk';
 
-/**
- * Handle incoming JSON-RPC requests, sent through `wallet_invokeSnap`.
- *
- * @param args - The request handler args as object.
- * @param args.origin - The origin of the request, e.g., the website that
- * invoked the snap.
- * @param args.request - A validated JSON-RPC request object.
- * @returns The result of `snap_dialog`.
- * @throws If the request method is not valid for this snap.
- */
-export const onRpcRequest: OnRpcRequestHandler = async ({
-  origin,
-  request,
-}) => {
-  switch (request.method) {
-    case 'hello':
-      return snap.request({
-        method: 'snap_dialog',
-        params: {
-          type: 'confirmation',
-          content: (
-            <Box>
-              <Text>
-                Hello, <Bold>{origin}</Bold>!
-              </Text>
-              <Text>
-                This custom confirmation is just for display purposes.
-              </Text>
-              <Text>
-                But you can edit the snap source code to make it do something,
-                if you want to!
-              </Text>
-            </Box>
-          ),
-        },
-      });
-    default:
-      throw new Error('Method not found.');
+export const onNameLookup: OnNameLookupHandler = async (request) => {
+  const { address, domain } = request;
+
+  if (domain) {
+    if (domain.startsWith('scheme:') || domain.startsWith('schm:')) {
+
+      console.log(`Got: ${domain}`);
+
+      return {
+        resolvedAddresses: [
+          {
+            resolvedAddress: "0x000000000000000000000000000000000000dEaD",
+            protocol: 'Ethereum',
+            domainName: "scheme:0xdead",
+          },
+        ],
+      };
+
+    } else if (domain.endsWith('.tld')) {
+      
+      console.log(`Got: ${domain}`); 
+
+      return {
+        resolvedAddresses: [
+          {
+            resolvedAddress: "0x000000000000000000000000000000000000dEaD",
+            protocol: 'Ethereum',
+            domainName: "0xdead.tld",
+          },
+        ],
+      };
+    }
   }
+
+  return null;
 };
